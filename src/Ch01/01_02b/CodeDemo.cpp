f8f1798246d11b06eefe3b66711d86f3e6c2469a
hugoAmosRust <40028673+hugoAmosRust@users.noreply.github.com>
2024-05-18 12:43:19 +0000

    First Hello World

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,16 @@
-// Learning C++ 
-// Exercise 01_02
-// Hello World, by Eduardo Corpeño 
+// Program Name:    Hello World
+// Description:     Writes "Hello World" to the console
+
+// Version  Date        Description                     Programmer
+// 0.0.0    2024-05-18  Initial version                 Hugo Rust                                
+
+// include libraries (pre-processor directive)
+#include <iostream>     // Standard C++ library write tect to screen and read from keyboard
+
+int main()
+{
+    std::cout << "Hello world!" << std::endl;       // :: = scope resolution operator
+                                                    // cout = character output
+                                                    // << = left-bound insertion operator
+    return(0);
+}
